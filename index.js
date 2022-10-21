3e877f96efdef144dd984d831ca853752c996fd2
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-20 20:05:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 document.addEventListener('keydown', function(event) {
     console.log(event.keyCode)
     document.getElementById("keyCodeH1").innerHTML = event.keyCode;
+    document.getElementById("typedDic").innerHTML += " " + event.key;
 });
+
+setInterval(function(){
+    document.getElementById("typedDic").style.color = randColor()
+},1000)
+
+function randColor(){
+    return '#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6)
+}
